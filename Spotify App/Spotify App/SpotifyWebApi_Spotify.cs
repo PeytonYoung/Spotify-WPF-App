e6226180b6f91e0a6c892e24e113560b9d83e538
Peyton Young <Peyton.Young@ou.edu>
2019-04-23 20:30:25 -0500

    Created the initial and added the API

---
+++ Spotify App/Spotify App/SpotifyWebApi_Spotify.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spotify_App
+{
+    class SpotifyWebApi_Spotify
+    {
+        public string client_id { get;  }
+        public string response_type { get; }
+
+
+
+    }
+}
